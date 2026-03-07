2a473ed8349be6eee2e7abcdfcd37da92aec0132
PJ1229 <peterjoe2005@gmail.com>
2026-03-07 13:19:46 -0500

    49 Group Anagrams

---
+++ 1-two-sum.cpp
@@ -0,0 +1,33 @@
+/*
+nums = [2,7,11,15], target = 9
+map
+indice : value
+0 : 2 
+1 : 7 
+2 : 11 
+3 : 15 
+
+target - 2 
+7 : 
+target - 7
+2 : 
+target - 11
+-2 : 
+target - 15
+6 : 
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> sum;
+        for(int i = 0; i < nums.size(); i++) {
+            int compliment = target - nums[i];
+            if (sum.find(compliment) != sum.end()) {
+                return {sum[compliment], i};
+            }
+            sum[nums[i]] = i;
+        }
+        return {};
+    }
+};
