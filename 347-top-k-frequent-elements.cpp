120d50ee88a1b7d983ac87224777b2c867bfdcd7
PJ1229 <peterjoe2005@gmail.com>
2026-03-08 17:51:05 -0400

    347 Top K Frequent Elements*

---
+++ 347-top-k-frequent-elements.cpp
@@ -0,0 +1,60 @@
+/*
+
+I USED OUTSIDE RESOURCES TO SOLVE THIS. REDO LATER
+
+Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+Output: [1,2]
+
+deque = {1}
+
+iterate through nums
+
+nums = 3 4 2 5 4 5 1 2 1 1 
+k = 4
+
+output = {4, 3, 2}
+
+use heap
+
+std::priority_queue<int, std::vector<int>, std::greater<int>> min_pq;
+*/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        // nums = [1,1,1,2,2,3]
+
+        unordered_map<int, int> hash; // num count
+
+        for(auto i : nums) {
+            hash[i]++;
+        }
+
+        /*
+        freq = {
+            1 → 3
+            2 → 2
+            3 → 1
+        }
+        */
+        vector<vector<int>> bucket(nums.size() + 1); 
+        // frequency table
+
+        for(auto& [num, count] : hash) {
+            bucket[count].push_back(num);
+        }
+
+        vector<int> output;
+
+        // not O(n^2) since it runs the exact time as there are elts n
+        for(int i = bucket.size() - 1; i >= 0; i--) {
+            for(int num : bucket[i]) {
+                output.push_back(num);
+                if(output.size() == k)
+                    return output;
+            }
+        }
+
+        return output;
+    }
+};
