86bc9859df20d97be8e9c32af55e8e50323e30ca
PJ1229 <peterjoe2005@gmail.com>
2026-03-10 13:39:36 -0400

    238 Product of Array Except Self*

---
+++ 238-product-of-array-except-self.cpp
@@ -0,0 +1,38 @@
+/*
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+nums
+[1,2,3,4]
+pre
+[1,1,2,6]
+pref[i] = nums[i - 1] * pref[i - 1];
+suff
+[24,12,4,1]
+suff[i] = nums[i + 1] * suff[i + 1];
+
+prefix and suffix
+cant use division operator
+*/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> pref(nums.size());
+        vector<int> suff(nums.size());
+        vector<int> output(nums.size());
+        pref[0] = 1;
+        suff[nums.size() - 1] = 1;
+
+        for(int i = 1; i < nums.size(); i++){
+            pref[i] = pref[i-1] * nums[i-1];
+        }
+        for(int i = nums.size() - 2; i >= 0; i--){
+            suff[i] = suff[i+1] * nums[i+1];
+        }
+        for(int i = 0; i < nums.size(); i++){
+            output[i] = suff[i] * pref[i];
+        }
+        return output;
+    }
+};
