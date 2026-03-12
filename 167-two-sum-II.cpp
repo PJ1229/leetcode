b663ecc0d358bc5d2d7d2eaf010446204517eb3c
PJ1229 <peterjoe2005@gmail.com>
2026-03-12 11:17:01 -0400

    167 Two Sum II

---
+++ 167-two-sum-II.cpp
@@ -0,0 +1,27 @@
+/*
+i was correct on the two pointer approach but i shouldve had the second index point to the end;
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return {left + 1, right + 1};
+            } 
+            else if (sum < target) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+
+        return {};
+    }
+};
