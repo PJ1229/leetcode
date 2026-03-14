221fa68157cd2c8c41cc16f10468f7b3f8e0f511
PJ1229 <peterjoe2005@gmail.com>
2026-03-13 20:19:51 -0400

    704 Binary Search

---
+++ 704-binary-search.cpp
@@ -0,0 +1,23 @@
+/*
+basically right but as i thought before you need an parameter to keep track
+*/
+
+class Solution {
+public:
+    int binarySearch(vector<int>& nums, int target, int left, int right) {
+        if (left > right) return -1;
+
+        int med = left + (right - left) / 2;
+
+        if (nums[med] == target) return med;
+        if (nums[med] > target) {
+            return binarySearch(nums, target, left, med - 1);
+        } else {
+            return binarySearch(nums, target, med + 1, right);
+        }
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, target, 0, nums.size() - 1);
+    }
+};
