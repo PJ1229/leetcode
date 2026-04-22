7c0357bde014014e39a0224469291d14cef68438
PJ1229 <peterjoe2005@gmail.com>
2026-04-22 18:09:27 -0400

    78 Subsets*

---
+++ 78-subsets-eecs281study.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    void backtrack(int i, vector<int>& nums, vector<int>& current, vector<vector<int>>& out) {
+        if(i == nums.size()) {
+            out.push_back(current);
+            return;
+        }
+
+        current.push_back(nums[i]);
+        backtrack(i+1, nums, current, out);
+
+        current.pop_back();
+
+        backtrack(i+1, nums, current, out);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> out;
+        vector<int> current;
+        backtrack(0, nums, current, out); //0, [1,2], [], []
+        return out;
+    }
+};
